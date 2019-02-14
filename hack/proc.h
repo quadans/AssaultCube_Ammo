171982a6c7e2d9f37ab5704c2c010d6da852ed5d
Dennis K <47493031+tongbal@users.noreply.github.com>
2019-02-14 10:01:51 +0100

    Add files via upload

---
+++ hack/proc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include <windows.h>
+#include <TlHelp32.h>
+#include <stdio.h>
+#include <tchar.h>
+#include <SDKDDKVer.h>
+
+DWORD GetProcId(const wchar_t* procName);
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
+
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
